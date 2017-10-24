391a1704055015bdaff9dcafdb5a18e6a3033749
Pramod Rao <spramod49@gmail.com>
2017-10-24 20:39:57 +0530

    Added comments. Made gif transparent.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
 //Basic user information api call
-$("#feed").hide();
-$("#profile").hide();
-$("img[src='loader.gif']").hide();
+$("#feed").hide();                    //This hides the feed section when page loads.
+$("#profile").hide();                 //This hides the profile section
+$("img[src='loader.gif']").hide();    //Hides the loader
 
-
+//This is event handler for getting the profile information once the "Get profile info" button is clicked.
 $("#profileButton").click(() => {
     $("#feed").hide();
-    $("#profile").hide();
     $("img[src='loader.gif']").show();
 
     var token = document.getElementById("token").value;
+    //The below statement checks if it has already loaded a profile, in which case it prevents from making another API call to facebook's graph api's
     if ($("div[name='name']>div>div.panel-body>ul")[0].children.length === 0) {
         $.ajax({
             url: "https://graph.facebook.com/me?fields=name,email,cover,favorite_athletes,favorite_teams,first_name,education,hometown,work,posts&access_token=" + token,
             type: 'GET',
             success: function(res) {
-
-                // console.log(res);
-                // console.log(res.email);
+                //If the request is a success, it loads the appropriate information split into self-explanatory functions
                 $("img[src='loader.gif']").hide();
                 loadCover(res.cover.source);
                 loadName(res.name, res.email, res.hometown);
@@ -25,11 +23,12 @@ $("#profileButton").click(() => {
                 favoriteTeams(res.favorite_teams);
                 workCard(res.work[0]);
                 educationCard(res.education);
+                //Makes the profile section visible
                 $("#profile").show(500);
             },
             error: function(res) {
-
                 var message = JSON.parse(res.responseText);
+                //Using the alertify.js jquery plugin to display the error message made on a call
                 alertify.alert("Oops!",message.error.message);
                 $("img[src='loader.gif']").hide();
             }
@@ -41,16 +40,19 @@ $("#profileButton").click(() => {
 
 });
 
+//Loads the cover photo of the user who made the call using his access token
 function loadCover(imageLink) {
     $("img[alt*='cover']").attr("src", imageLink);
     $("header>div").attr("style", "background-image: url('" + imageLink + "')");
     // background-image: url('images/img.jpg')
 }
 
+//Loads the name, email and hometown
 function loadName(name, email, hometown) {
     $("div[name='name'] ul").append("<li><strong>Name:</strong> " + name + " </li><li><strong>Email: </strong>" + email + "</li><li><strong>Hometown: </strong>" + hometown.name + "</li>");
 }
 
+//Loads the favorite athletes of a person
 function favoriteAthletes(athletes) {
     athletes.forEach((athlete, i) => {
         if (i < 16) {
@@ -60,33 +62,37 @@ function favoriteAthletes(athletes) {
     });
 }
 
+//Loads the favorite teams of a person
 function favoriteTeams(teams) {
     teams.forEach((team, i) => {
         $("div[name*='teams']>div>div>ul").append("<li>" + team.name + "</li>");
     })
 }
 
+//Loads the workplace of a person
 function workCard(work) {
     $("div[name*=work]>div>div>ul").append(`<li><strong>Employer: </strong>` + work.employer.name + `</li>
 											<li><strong>Location: </strong>` + work.location.name + `</li>
 											<li><strong>Position: </strong>` + work.position.name + `</li>`); //"<p>I work for "+work.employer.name+" at "+work.location.name+" as a "+work.position.name+"</p>")
 }
 
+//Loads the education schools that the user who made the call went to
 function educationCard(education) {
 
     education.forEach((e, i) => {
         $("div[name*=education]>div>div>ul").append("<li>" + e.school.name + "</li>");
-        if (e.concentration) {
-            e.concentration.forEach((mainSubjects) => {
-                // $("div[name*=education]>div>div>ul").append("<li> Main subject(s): "+mainSubjects.name+"</p>");
-            })
+        // if (e.concentration) {
+        //     e.concentration.forEach((mainSubjects) => {
+        //         // $("div[name*=education]>div>div>ul").append("<li> Main subject(s): "+mainSubjects.name+"</p>");
+        //     })
         }
-    })
+    );
 }
 
+
+//Event handler for "Get the latest feed button"
 $('#feedsButton').on('click', () => {
-    $("#profile").hide();
-    // $("#feed").show(500);
+    $("#profile").hide(); //Hides the profile section as the user probably wants to get the latest feed section by clicking this button
     $("img[src='loader.gif']").show();
     var token = document.getElementById("token").value;
     if ($("#feed>.row")[0].children.length === 0) {
@@ -97,14 +103,11 @@ $('#feedsButton').on('click', () => {
                 $("img[src='loader.gif']").hide();
                 populateFeed(res.data, res.paging);
                 $("#feed").show(500);
-                // console.log(res);
             },
             error: function(res) {
                 var message = JSON.parse(res.responseText);
-                // console.log(message.error.message);
                 alertify.alert(message.error.message);
                 $("img[src='loader.gif']").hide();
-                // $("#feed").show(500);
             }
         });
     } else {
@@ -113,6 +116,7 @@ $('#feedsButton').on('click', () => {
     }
 });
 
+//This functions populates the latest feed of the user who made the call
 function populateFeed(feeds, paging) {
     feeds.forEach((feed) => {
         if (feed.story) {
